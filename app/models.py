b7ccb58305be5b4e2eaca0258e39584798566315
noattongi <noattongi@gmail.com>
2025-02-04 22:14:36 -0800

    initialize and setup for paanga-no app

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from django.db.models import Model
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+# class Loan(Model):
+
+# class Lender(AbstractUser):
+
+# class Investor(AbstractUser):
+
+# class Admin(AbstractUser):
